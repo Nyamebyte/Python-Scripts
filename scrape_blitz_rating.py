2652366f3e242f9df839d6d38492677befe54d8e
Bernard Anhwere <126365347+Nyamebyte@users.noreply.github.com>
2023-10-16 15:40:48 +0000

    Update scrape_blitz_rating.py

--- scrape_blitz_rating.py
+++ scrape_blitz_rating.py
@@ -1,21 +1,22 @@
 import lichess
 
 
-def get_rating(user):
+def get_rating(user, control):
     myClient = lichess.Client()
-    rating = myClient.get_stats(f'{user}', 'blitz')
+    rating = myClient.get_stats(f'{user}', f'{control}')
     rating_figure = rating['perf']['glicko']['rating']
     return rating_figure
 
 
 def main():
+    control = input("What time control? ")
     print('Type the username of player')
     print('Type "stop" to exit')
     while True:
         user = input('> ')
         if user == 'stop':
             break
-        rating = get_rating(user)
+        rating = get_rating(user, control)
         print(f"{user}'s rating is {rating}")
 
 
